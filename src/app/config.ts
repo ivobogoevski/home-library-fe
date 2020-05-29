cafdbe5fd54abc83027ec407eea5384e884db372
Ivo Bogoevski <as@interworks.local>
2020-05-29 12:29:08 +0200

    added Azure environment

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,4 @@
 export const config = {
-  apiUrl: 'http://homelibrary-env.eba-tpasr2x2.us-east-2.elasticbeanstalk.com/api'
+  // apiUrl: 'http://homelibrary-env.eba-tpasr2x2.us-east-2.elasticbeanstalk.com/api'
+  apiUrl: 'https://home-library.azurewebsites.net/api'
 };
